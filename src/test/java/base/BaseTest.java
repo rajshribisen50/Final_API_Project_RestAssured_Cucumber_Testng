07d7a88e6e5757a171d05ce2e1eed3556d9a1644
Rajshri <rajshribisen50@gmail.com>
2026-02-07 03:20:27 +0530

    fixed allure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,10 +8,12 @@ Central auth & headers
  */
 
 import config.ConfigReader;
+import io.qameta.allure.testng.AllureTestNg;
 import io.restassured.RestAssured;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
-
+@Listeners({AllureTestNg.class, listeners.TestListener.class})
 public class BaseTest {
 
 
